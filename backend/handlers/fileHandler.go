f09d8c375b072cc86eff963dcdeaf2166b216007
Gabriel Gerhardt <gabrielgerhardt27@gmail.com>
2025-07-02 18:05:39 -0300

    file handler

--- backend/handlers/fileHandler.go
+++ backend/handlers/fileHandler.go
@@ -1,1 +1,31 @@
 package handlers
+
+import (
+	"backend/models"
+
+	"github.com/gin-gonic/gin"
+	"gorm.io/gorm"
+)
+
+type FileController struct {
+	DB *gorm.DB
+}
+
+func newFileController(db *gorm.DB) *FileController {
+	return &FileController{DB: db}
+
+}
+
+func (u *FileController) PostFile(c *gin.Context) {
+	var file models.File
+
+	if err := c.BindJSON(&file); err != nil {
+		c.JSON(400, gin.H{"Error in parsing file to json": err.Error()})
+	}
+
+	if err := u.DB.Create(&file).Error; err != nil {
+		c.JSON(500, gin.H{"Error in writing file to database": err.Error()})
+		return
+	}
+	c.JSON(200, file)
+}
